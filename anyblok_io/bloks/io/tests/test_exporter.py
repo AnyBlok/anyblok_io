aaf179dee2577f3625074df48b9ba1706c8a6454
jssuzanne <js.suzanne@gmail.com>
2020-05-05 15:04:45 +0200

    Refactored io/test_exporter

--- anyblok_io/bloks/io/tests/test_exporter.py
+++ anyblok_io/bloks/io/tests/test_exporter.py
@@ -5,10 +5,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+import pytest
 from ..exceptions import ExporterException
 
 
+@pytest.mark.usefixtures('rollback_registry')
 class TestExporter:
+
+    @pytest.fixture(autouse=True)
+    def transact(self, rollback_registry):
+        self.registry = rollback_registry
 
     def test_get_external_id(self):
         Exporter = self.registry.IO.Exporter
@@ -16,7 +22,7 @@ class TestExporter:
         val2 = Exporter.get_external_id(Exporter.__registry_name__)
         val1 = val1.split('_')
         val1 = '_'.join([val1[0], str(int(val1[1]) + 1)])
-        self.assertEqual(val1, val2)
+        assert val1 == val2
 
     def test_get_key_for_mapping(self):
         Exporter = self.registry.IO.Exporter
@@ -25,7 +31,7 @@ class TestExporter:
         val1 = Exporter.get_external_id(Blok.__registry_name__)
         val1 = val1.split('_')
         val1 = '_'.join([val1[0], str(int(val1[1]) + 1)])
-        self.assertEqual(Exporter.get_key_mapping(entry), val1)
+        assert Exporter.get_key_mapping(entry) == val1
 
     def test_get_key_for_mapping_2nd_time(self):
         Exporter = self.registry.IO.Exporter
@@ -34,12 +40,12 @@ class TestExporter:
         val1 = Exporter.get_external_id(Blok.__registry_name__)
         val1 = val1.split('_')
         val1 = '_'.join([val1[0], str(int(val1[1]) + 1)])
-        self.assertEqual(Exporter.get_key_mapping(entry), val1)
-        self.assertEqual(Exporter.get_key_mapping(entry), val1)
+        assert Exporter.get_key_mapping(entry) == val1
+        assert Exporter.get_key_mapping(entry) == val1
 
     def test_export_with_entries_doesnt_come_from_model(self):
         Exporter = self.registry.IO.Exporter
         Blok = self.registry.System.Blok
         exporter = Exporter(model=Blok.__registry_name__)
-        with self.assertRaises(ExporterException):
+        with pytest.raises(ExporterException):
             exporter.run([exporter])
