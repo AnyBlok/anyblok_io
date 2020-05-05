2a59a21c82e10d77bd614c94e2bf1a4b931c0116
jssuzanne <js.suzanne@gmail.com>
2020-05-05 15:17:56 +0200

    Refactored io/test_importer_formater

--- anyblok_io/bloks/io/tests/test_importer_formater.py
+++ anyblok_io/bloks/io/tests/test_importer_formater.py
@@ -5,10 +5,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
+import pytest
 from json import dumps
 
 
+@pytest.mark.usefixtures('rollback_registry')
 class TestImporterFormater:
+
+    @pytest.fixture(autouse=True)
+    def transact(self, rollback_registry):
+        self.registry = rollback_registry
 
     def get_value(self, value, ctype, external_id=False, model=None):
         return self.registry.IO.Importer().str2value(value, ctype,
@@ -18,96 +24,96 @@ class TestImporterFormater:
     def test_datetime(self):
         from datetime import datetime
         value = self.get_value("2015-05-01 17:52:01", "DateTime")
-        self.assertTrue(isinstance(value, datetime))
-        self.assertEqual(value.year, 2015)
-        self.assertEqual(value.month, 5)
-        self.assertEqual(value.day, 1)
+        assert isinstance(value, datetime)
+        assert value.year == 2015
+        assert value.month == 5
+        assert value.day == 1
 
     def test_decimal(self):
         from decimal import Decimal
         value = self.get_value("1.5", "Decimal")
-        self.assertTrue(isinstance(value, Decimal))
-        self.assertEqual(value, Decimal("1.5"))
+        assert isinstance(value, Decimal)
+        assert value == Decimal("1.5")
 
     def test_float(self):
         value = self.get_value("1.5", "Float")
-        self.assertTrue(isinstance(value, float))
-        self.assertEqual(value, 1.5)
+        assert isinstance(value, float)
+        assert value == 1.5
 
     def test_json(self):
         value = self.get_value(dumps({'id': 1}), "Json")
-        self.assertTrue(isinstance(value, dict))
-        self.assertEqual(value, {'id': 1})
+        assert isinstance(value, dict)
+        assert value == {'id': 1}
 
     def test_time(self):
         from datetime import time
         value = self.get_value("17:52:01", "Time")
-        self.assertTrue(isinstance(value, time))
-        self.assertEqual(value.hour, 17)
+        assert isinstance(value, time)
+        assert value.hour == 17
 
     def test_big_interger(self):
         value = self.get_value("17", "BigInteger")
-        self.assertTrue(isinstance(value, int))
-        self.assertEqual(value, 17)
+        assert isinstance(value, int)
+        assert value == 17
 
     def test_boolean(self):
         for v in ('1', 'true', 'True'):
             value = self.get_value(v, "Boolean")
-            self.assertTrue(isinstance(value, bool))
-            self.assertEqual(value, True)
+            assert isinstance(value, bool)
+            assert value is True
 
         for v in ('0', 'false', 'False'):
             value = self.get_value(v, "Boolean")
-            self.assertTrue(isinstance(value, bool))
-            self.assertEqual(value, False)
+            assert isinstance(value, bool)
+            assert value is False
 
     def test_date(self):
         from datetime import date
         value = self.get_value("2015-05-01", "Date")
-        self.assertTrue(isinstance(value, date))
-        self.assertEqual(value.year, 2015)
-        self.assertEqual(value.month, 5)
-        self.assertEqual(value.day, 1)
+        assert isinstance(value, date)
+        assert value.year == 2015
+        assert value.month == 5
+        assert value.day == 1
 
     def test_interger(self):
         value = self.get_value("10", "Integer")
-        self.assertTrue(isinstance(value, int))
-        self.assertEqual(value, 10)
+        assert isinstance(value, int)
+        assert value == 10
 
     def test_interval(self):
         from datetime import timedelta
         value = self.get_value("10", "Interval")
-        self.assertTrue(isinstance(value, timedelta))
-        self.assertEqual(value.seconds, 10)
+        assert isinstance(value, timedelta)
+        assert value.seconds == 10
 
     def test_large_binary(self):
         from os import urandom
         blob = urandom(100)
         value = self.get_value(blob, "Large_Binary")
-        self.assertTrue(isinstance(value, bytes))
-        self.assertEqual(value, blob)
+        assert isinstance(value, bytes)
+        assert value == blob
 
     def test_selection(self):
         value = self.get_value("selection", "Selection")
-        self.assertTrue(isinstance(value, str))
-        self.assertEqual(value, "selection")
+        assert isinstance(value, str)
+        assert value == "selection"
 
     def test_string(self):
         value = self.get_value("selection", "String")
-        self.assertTrue(isinstance(value, str))
-        self.assertEqual(value, "selection")
+        assert isinstance(value, str)
+        assert value == "selection"
 
     def test_text(self):
         value = self.get_value("selection", "Text")
-        self.assertTrue(isinstance(value, str))
-        self.assertEqual(value, "selection")
+        assert isinstance(value, str)
+        assert value == "selection"
 
     def test_many2many(self):
         pks = dict(name='Model.System.Model')
         model = self.registry.System.Model.from_primary_keys(**pks)
         value = self.get_value(
             dumps([pks]), "Many2Many", model="Model.System.Model")
-        self.assertEqual(value, [model])
+        assert value == [model]
 
     def test_many2many_multi_value(self):
         pks1 = dict(name='Model.System.Model')
@@ -116,7 +122,7 @@ class TestImporterFormater:
         model2 = self.registry.System.Model.from_primary_keys(**pks2)
         value = self.get_value(
             dumps([pks1, pks2]), "Many2Many", model="Model.System.Model")
-        self.assertEqual(value, [model1, model2])
+        assert value == [model1, model2]
 
     def test_many2many_external_ids(self):
         key = 'formater_mapping'
@@ -125,7 +131,7 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key, model)
         value = self.get_value(dumps([key]), "Many2Many",
                                external_id=True, model="Model.System.Model")
-        self.assertEqual(value, [model])
+        assert value == [model]
 
     def test_many2many_multi_value_external_ids(self):
         key1 = 'formater_mapping1'
@@ -138,14 +144,14 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key2, model2)
         value = self.get_value(dumps([key1, key2]), "Many2Many",
                                external_id=True, model="Model.System.Model")
-        self.assertEqual(value, [model1, model2])
+        assert value == [model1, model2]
 
     def test_one2many(self):
         pks = dict(name='Model.System.Model')
         model = self.registry.System.Model.from_primary_keys(**pks)
         value = self.get_value(
             dumps([pks]), "One2Many", model="Model.System.Model")
-        self.assertEqual(value, [model])
+        assert value == [model]
 
     def test_one2many_multi_value(self):
         pks1 = dict(name='Model.System.Model')
@@ -154,7 +160,7 @@ class TestImporterFormater:
         model2 = self.registry.System.Model.from_primary_keys(**pks2)
         value = self.get_value(
             dumps([pks1, pks2]), "One2Many", model="Model.System.Model")
-        self.assertEqual(value, [model1, model2])
+        assert value == [model1, model2]
 
     def test_one2many_external_ids(self):
         key = 'formater_mapping'
@@ -163,17 +169,17 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key, model)
         value = self.get_value(dumps([key]), "One2Many", external_id=True,
                                model="Model.System.Model")
-        self.assertEqual(value, [model])
+        assert value == [model]
 
     def test_one2many_external_ids_is_none_1(self):
         value = self.get_value(dumps(None), "One2Many", external_id=True,
                                model="Model.System.Model")
-        self.assertEqual(value, [])
+        assert value == []
 
     def test_one2many_external_ids_is_none_2(self):
         value = self.get_value(dumps([]), "One2Many", external_id=True,
                                model="Model.System.Model")
-        self.assertEqual(value, [])
+        assert value == []
 
     def test_one2many_multi_value_external_ids(self):
         key1 = 'formater_mapping1'
@@ -186,14 +192,14 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key2, model2)
         value = self.get_value(dumps([key1, key2]), "One2Many",
                                external_id=True, model="Model.System.Model")
-        self.assertEqual(value, [model1, model2])
+        assert value == [model1, model2]
 
     def test_many2one(self):
         pks = dict(name='Model.System.Model')
         model = self.registry.System.Model.from_primary_keys(**pks)
         value = self.get_value(
             dumps(pks), "Many2One", model="Model.System.Model")
-        self.assertEqual(value, model)
+        assert value == model
 
     def test_many2one_external_ids(self):
         key = 'formater_mapping'
@@ -202,19 +208,19 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key, model)
         value = self.get_value(key, "Many2One", external_id=True,
                                model="Model.System.Model")
-        self.assertEqual(value, model)
+        assert value == model
 
     def test_many2one_external_ids_with_none_value(self):
         value = self.get_value(None, "Many2One", external_id=True,
                                model="Model.System.Model")
-        self.assertIsNone(value)
+        assert value is None
 
     def test_one2one(self):
         pks = dict(name='Model.System.Model')
         model = self.registry.System.Model.from_primary_keys(**pks)
         value = self.get_value(
             dumps(pks), "One2One", model="Model.System.Model")
-        self.assertEqual(value, model)
+        assert value == model
 
     def test_one2one_external_ids(self):
         key = 'formater_mapping'
@@ -223,4 +229,4 @@ class TestImporterFormater:
         self.registry.IO.Mapping.set(key, model)
         value = self.get_value(key, "One2One", external_id=True,
                                model="Model.System.Model")
-        self.assertEqual(value, model)
+        assert value == model
