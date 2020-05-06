423b945bafb2bca905f39f8f7faeac767ffea83b
jssuzanne <js.suzanne@gmail.com>
2020-05-06 10:04:17 +0200

    Merged unittest

--- anyblok_io/tests/test_blok.py
+++ anyblok_io/tests/test_blok.py
@@ -6,21 +6,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file,You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-import pytest
+import pytest  # noqa
 
 
-class TestBlokRequired:
+class TestBlok:
 
-    @pytest.fixture(autouse=True)
-    def transact(self, request, registry_testblok):
-        transaction = registry_testblok.begin_nested()
-        request.addfinalizer(transaction.rollback)
-        return
-
-    def test_import_file_csv(self, registry_testblok):
-        registry_testblok.upgrade(install=('test-io-blok1',))
-        assert registry_testblok.Exemple.query().count() == 3
-
-    def test_import_file_xml(self, registry_testblok):
-        registry_testblok.upgrade(install=('test-io-blok2',))
-        assert registry_testblok.Exemple.query().count() == 3
+    def test_import_file(self, registry_testblok):
+        registry_testblok.upgrade(install=('test-io-blok1', 'test-io-blok2'))
+        assert registry_testblok.Exemple.query().count() == 6
